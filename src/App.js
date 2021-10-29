0d7adddcd786274289fe60f1cc8c352e510cfa3f
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-10-29 20:29:23 +0300

    Mounted the created componets on the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import Theme from './Theme';
+import Map from './components/Map';
+import AccessPanel from './components/AccessPanel';
+import StoreContext, { initialState } from './context/Store';
 
 export default function App() {
-  return <div></div>;
+  const classes = useStyles();
+  const [store, setStore] = useState(initialState);
+
+  return (
+    <StoreContext.Provider value={{ store, setStore }}>
+      <Theme>
+        <div className={classes.root}>
+          <Map />
+          <AccessPanel />
+        </div>
+      </Theme>
+    </StoreContext.Provider>
+  );
 }
+
+const useStyles = makeStyles({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    position: 'relative',
+    overflow: 'hidden',
+  },
+});
