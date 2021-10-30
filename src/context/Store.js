97aa3092de9f56f410303fa17f6171fd9d97131b
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-10-30 23:11:59 +0300

    Added to store accessible filter and map ref

--- src/context/Store.js
+++ src/context/Store.js
@@ -1,12 +1,14 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 export const initialState = {
-    records: [],
-    filter: {
-        ATM_Type: '',
-        bankName: '',
-    }
-}
+  records: [],
+  filter: {
+    ATM_Type: '',
+    bankName: '',
+    accessible: '',
+  },
+  mapRef: null,
+};
 
 const StoreContext = createContext(null);
 
