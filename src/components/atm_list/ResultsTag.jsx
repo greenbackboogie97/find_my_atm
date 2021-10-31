a3dfa76733dea587ae95621a79a37ff874e4178c
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-10-31 08:23:05 +0200

    results tag dedicated component

---
+++ src/components/atm_list/ResultsTag.jsx
@@ -0,0 +1,25 @@
+import { makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function ResultsTag(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography className={classes.results} variant='caption'>
+        {props.number} תוצאות
+      </Typography>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    direction: 'rtl',
+    marginBottom: theme.spacing(2),
+  },
+  results: {
+    width: 'fit-content',
+    padding: theme.spacing(1),
+  },
+}));
