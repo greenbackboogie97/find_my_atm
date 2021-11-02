0185d0418e9cdffbf958bf875d5a561b98d0bc7f
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-11-02 09:06:58 +0200

    pagination for the API

--- src/services/AutoDevicesAPI.js
+++ src/services/AutoDevicesAPI.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
-const getATMs = async (city) => {
+const getAllATMS = async (page) => {
+  const data = await axios.post(
+    `https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&offset=${
+      page * 100
+    }`
+  );
+
+  return data.data.result.records;
+};
+
+const getATMsByCity = async (city) => {
   const data = await axios.post(
     `https://data.gov.il/api/3/action/datastore_search?resource_id=b9d690de-0a9c-45ef-9ced-3e5957776b26&q=${city}&limit=500`
   );
@@ -8,4 +18,4 @@ const getATMs = async (city) => {
   return data.data.result.records;
 };
 
-export default getATMs;
+export { getAllATMS, getATMsByCity };
