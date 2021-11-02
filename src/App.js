961193b8afdef00db942395735d548e41ed90a8e
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-11-02 09:05:37 +0200

    Removed  filteredRecords context, added pagination

--- src/App.js
+++ src/App.js
@@ -4,52 +4,29 @@ import Theme from './Theme';
 import Map from './components/Map';
 import AccessPanel from './components/AccessPanel';
 import StoreContext, { initialState } from './context/Store';
-import FilteredRecordsContext from './context/FilteredRecordsContext';
+import { getAllATMS } from './services/AutoDevicesAPI';
 
 export default function App() {
   const classes = useStyles();
   const [store, setStore] = useState(initialState);
-  const [filteredRecords, setFilteredRecords] = useState(store.records);
 
   useEffect(() => {
-    if (!store.records.length) return;
-    setFilteredRecords(store.records);
-  }, [store.records]);
-
-  useEffect(() => {
-    setFilteredRecords(
-      store.records.filter((record) => {
-        return (
-          (store.filter.accessible
-            ? store.filter.accessible === 'כן'
-              ? record.Handicap_Access === 'כן'
-              : record.Handicap_Access === 'לא'
-            : true) &&
-          (store.filter.ATM_Type
-            ? store.filter.ATM_Type === 'מכשיר מידע/או מתן הוראות\n'
-              ? record.ATM_Type === 'מכשיר מידע/או מתן הוראות\n'
-              : record.ATM_Type === 'משיכת מזומן'
-            : true) &&
-          (store.filter.bankName
-            ? store.filter.bankName === record.Bank_Name
-            : true)
-        );
-      })
+    getAllATMS(1).then((records) =>
+      setStore((prev) => ({
+        ...prev,
+        records: { page: prev.records.page++, list: records },
+      }))
     );
-  }, [store.filter, store.records]);
+  }, []);
 
   return (
     <StoreContext.Provider value={{ store, setStore }}>
-      <FilteredRecordsContext.Provider
-        value={{ filteredRecords, setFilteredRecords }}
-      >
-        <Theme>
-          <div className={classes.root}>
-            <Map />
-            <AccessPanel />
-          </div>
-        </Theme>
-      </FilteredRecordsContext.Provider>
+      <Theme>
+        <div className={classes.root}>
+          <Map />
+          <AccessPanel />
+        </div>
+      </Theme>
     </StoreContext.Provider>
   );
 }
